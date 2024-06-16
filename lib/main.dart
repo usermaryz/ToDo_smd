f57b7c19eb6ca3dd1a7c4d782b2c97bb5bd5cb1d
Mary <zemlyanskayamariaa@gmail.com>
2024-06-17 01:18:03 +0300

    adding name and icon

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Мои дела',
+      title: 'To Do List',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
